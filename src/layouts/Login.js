f940dea2da181e1c1a8729ea170a3f4181a78b29
Duc <namlun96@gmail.com>
2021-06-11 22:11:51 +0700

    update

--- src/layouts/Login.js
+++ src/layouts/Login.js
@@ -20,7 +20,7 @@ const LoginLayout = (props) => {
             title: `Hi ${res.data[0].firstName}!`,
             text: "Let's checkout some movie 😉",
             icon: "success",
-            // timer: 3000,
+            timer: 3000,
             timerProgressBar: true,
             backdrop: `
             rgba(3, 86, 252,0.2)
