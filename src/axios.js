9f0cf75b3ceda0ed0b274e25efe0ef3b763c6154
Duc <namlun96@gmail.com>
2021-06-14 00:45:15 +0700

    back remote json-server

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,8 @@ import Axios from "axios";
 // import env from "react-dotenv";
 
 export const axios = Axios.create({
-  // baseURL: "https://d1d4p.sse.codesandbox.io/",
-  baseURL: `http://localhost:3001`,
+  baseURL: "https://d1d4p.sse.codesandbox.io",
+  // baseURL: `http://localhost:3001`,
   headers: { Auth: "VDB" },
   timeout: 3000,
 });
