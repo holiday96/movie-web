6fafbf676f6e33fa81e67b1cec38183ffc567de7
Duc <namlun96@gmail.com>
2021-06-14 00:25:20 +0700

    udpate origin local server

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 
 export const axios = Axios.create({
   // baseURL: "https://d1d4p.sse.codesandbox.io/",
-  baseURL: `https://d1d4p.sse.codesandbox.io/`,
+  baseURL: `http://localhost:3001`,
   headers: { Auth: "VDB" },
   timeout: 3000,
 });
