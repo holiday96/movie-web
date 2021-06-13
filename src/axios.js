5aa6acd7ed9d7159f2c0e3590c540cc6aacaee45
Duc <namlun96@gmail.com>
2021-06-13 22:20:31 +0700

    replace baseURL axios by env.Local_host_api_url

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,9 @@
 import Axios from "axios";
+import env from "react-dotenv";
 
 export const axios = Axios.create({
   // baseURL: "https://d1d4p.sse.codesandbox.io/",
-  baseURL: "http://localhost:3001/",
+  baseURL: `${env.LOCAL_HOST_API_URL}`,
   headers: { Auth: "VDB" },
   timeout: 3000,
 });
